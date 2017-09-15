af9eea85e093edbb5df1fe0b5fb53b1d357820ea
yxzjerryfish <yxzjerryfish@hotmail.com>
2017-09-15 23:09:30 +0800

    if and while

--- python/dictionary.py
+++ python/dictionary.py
@@ -1,4 +1,12 @@
-age = 20
-if age>=18:
-    print('your age is ',age)
-    print('adult')
+age=1
+while age>0:
+    age = input('please input age:')
+    if age>=18:
+        print('your age is ',age)
+        print('adult')
+    elif age>=12 :
+        print('your age is',age)
+        print('teenager')
+    else:
+        print('your age is ',age)
+        print('kid')
